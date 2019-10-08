9b207cf42930114c2b30d7d6265129b48006b42a
CORP\korshunova.natalia <natalia.korshunova@devexpress.com>
2019-10-08 17:51:01 +0300

    fix T815883

--- CS/CustomNotifications.Module/BusinessObjects/CustomNotificationsDbContext.cs
+++ CS/CustomNotifications.Module/BusinessObjects/CustomNotificationsDbContext.cs
@@ -1,22 +1,17 @@
 using System;
-using System.Data;
-using System.Linq;
-using System.Data.Entity;
-using System.Data.Common;
-using System.Data.Entity.Core.Objects;
-using System.Data.Entity.Infrastructure;
+using System.Collections.Generic;
 using System.ComponentModel;
-using DevExpress.ExpressApp.EF.Updating;
-using DevExpress.Persistent.BaseImpl.EF;
-using DevExpress.Persistent.Base.General;
 using System.ComponentModel.DataAnnotations.Schema;
-
-using DevExpress.Persistent.Base;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
 using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.EF.Updating;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Base.General;
 
-namespace  CustomNotifications.Module.BusinessObjects {
-	public class CustomNotificationsDbContext : DbContext {
+namespace CustomNotifications.Module.BusinessObjects {
+    public class CustomNotificationsDbContext : DbContext {
 		public CustomNotificationsDbContext(String connectionString)
 			: base(connectionString) {
 		}
@@ -27,39 +22,60 @@ namespace  CustomNotifications.Module.BusinessObjects {
         public DbSet<Task> Tasks { get; set; }
 	}
     [DefaultClassOptions]
-    public class Task : ISupportNotifications, IXafEntityObject {
+    public class Task : ISupportNotifications, IXafEntityObject, INotifyPropertyChanged {
         public int Id { get; private set; }
-        public string Subject { get; set; }
-        public DateTime DueDate { get; set; }
+        private string subject;
+        public string Subject {
+            get { return subject; }
+            set { SetProperty<string>(ref subject, value); }
+        }
+        private DateTime dueDate;
+        public DateTime DueDate {
+            get { return dueDate; }
+            set { SetProperty<DateTime>(ref dueDate, value);}
+        }
 
         #region ISupportNotifications members
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null) {
+            if(!EqualityComparer<T>.Default.Equals(field, value)) {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
         private DateTime? alarmTime;
         [Browsable(false)]
         public DateTime? AlarmTime {
             get { return alarmTime; }
             set {
-                alarmTime = value;
+                SetProperty<DateTime?>(ref alarmTime, value);
                 if (value == null) {
                     RemindIn = null;
                     IsPostponed = false;
                 }
             }
         }
+        private bool isPostponed;
         [Browsable(false)]
-        public bool IsPostponed { get; set; }
+        public bool IsPostponed {
+            get { return isPostponed;}
+            set { SetProperty<bool>(ref isPostponed, value);}
+        }
         [Browsable(false), NotMapped]
         public string NotificationMessage {
             get { return Subject; }
         }
-        public TimeSpan? RemindIn { get; set; }
+        private TimeSpan? remindIn;
+        public TimeSpan? RemindIn {
+            get { return remindIn; }
+            set { SetProperty<TimeSpan?>(ref remindIn, value); }
+        }
        
         [Browsable(false), NotMapped]
         public object UniqueId {
             get { return Id; }
         }
         #endregion
-
-
         
         #region IXafEntityObject members
         public void OnCreated() { }
@@ -78,6 +94,13 @@ namespace  CustomNotifications.Module.BusinessObjects {
                 IsPostponed = false;
             }
         }
+        #endregion
+
+        #region INotifyPropertyChanged members
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
     }
 }
